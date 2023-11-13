d9a1f6cc9cfe46b1cd1e58ba8aca801d70113466
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-13 21:27:38 +0100

    fix: appendChild instead of append

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const input = document.querySelector("input");
-const array = []; /* errore: questa variabile sia semanticamente che logicamente deve essere un array*/
+const array = []; /* errore 1: questa variabile sia semanticamente che logicamente deve essere un array*/
 
 input.addEventListener("keypress", function (event) {
-  if (event.code !== "Enter") return; /* errore: Corretto l'uso dell'operatore di confronto in if (event.code != 'Enter') utilizzando !== */
-  if (input.value.length === 0) return; /* errore: corretto il test sulla lunghezza dell'input a if (input.value.length === 0) */
+  if (event.code !== "Enter") return; /* errore 2: Corretto l'uso dell'operatore di confronto in if (event.code != 'Enter') utilizzando !== */
+  if (input.value.length === 0) return; /* errore 3: corretto il test sulla lunghezza dell'input a if (input.value.length === 0) */
 
-  array.push(input.value); /* errore: il metodo per inserire un elemento in un array è push e non add */
+  array.push(input.value); /* errore 4: il metodo per inserire un elemento in un array è push e non add */
 
   const li = document.createElement("li");
   li.classList.add("list-group-item");
-  li.textContent = input.value; /* errore: l'attributo corretto di li non è text ma textContent */
-  document.querySelector("ul").append(li); /* errore: non si può usare il metodo push per aggiungere un elemento html con js */
+  li.textContent = input.value; /* errore 5: l'attributo corretto di li non è text ma textContent */
+  document.querySelector("ul").appendChild(li); /* errore 6: non si può usare il metodo push per aggiungere un elemento html con js */
 
   let counter = "";
   let item = array[0];
-  let max = 1; /* errore: non si può impostare max a const e poi assegnarli come valore una variabile let a riga 32 */
+  let max = 1; /* errore 7: non si può impostare max a const e poi assegnarli come valore una variabile let a riga 32 */
   const elems = [];
 
   for (let i = 0; i < array.length; i++) {
@@ -37,7 +37,7 @@ input.addEventListener("keypress", function (event) {
     counter = 0;
   }
 
-  const alert = document.querySelector(".alert"); /* errore: il selettore corretto è query selector e manca il punto prima della classe */
+  const alert = document.querySelector(".alert"); /* errore 8: il selettore corretto è query selector e manca il punto prima della classe */
 
   alert.classList.remove("d-none");
   alert.classList.add("d-flex");
@@ -45,5 +45,5 @@ input.addEventListener("keypress", function (event) {
   alert.querySelector("span:first-child").textContent = item;
   alert.querySelector("span:last-child").textContent = max;
 
-  console.log(`${item} trovato ${max} volte`); /* errore: c'erano gli apici normali e non i backtick */
+  console.log(`${item} trovato ${max} volte`); /* errore 9: c'erano gli apici normali e non i backtick */
 });
