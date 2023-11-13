d6ebd8b9a2cbcbf215373fb4cdd9968882180e74
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-13 21:21:42 +0100

    fix: js bugs

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-const input = document.querySelector('input');
-const array = null;
+const input = document.querySelector("input");
+const array = []; /* errore: questa variabile sia semanticamente che logicamente deve essere un array*/
 
-input.addEventListener('keypress', function(event) {
+input.addEventListener("keypress", function (event) {
+  if (event.code !== "Enter") return; /* errore: Corretto l'uso dell'operatore di confronto in if (event.code != 'Enter') utilizzando !== */
+  if (input.value.length === 0) return; /* errore: corretto il test sulla lunghezza dell'input a if (input.value.length === 0) */
 
-    if (event.code != 'Enter')    return;
-    if (input.value.length == '') return;
+  array.push(input.value); /* errore: il metodo per inserire un elemento in un array è push e non add */
 
-    array.add(input.value);
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.textContent = input.value; /* errore: l'attributo corretto di li non è text ma textContent */
+  document.querySelector("ul").append(li); /* errore: non si può usare il metodo push per aggiungere un elemento html con js */
 
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+  let counter = "";
+  let item = array[0];
+  let max = 1; /* errore: non si può impostare max a const e poi assegnarli come valore una variabile let a riga 32 */
+  const elems = [];
 
-    let counter = '';
-    let item    = array[0];
-    const max   = 1;
-    const elems = [];
+  for (let i = 0; i < array.length; i++) {
+    let val = array[i];
 
-    for (let i = 0; i < array; i++) {
-        let val = array[i];
-
-        if (!elems[val]) {
-            elems[val] = 1;
-        } else {
-            elems[val]++;
-        }
-
-        for (let j = i; j < array.length; i++) {
-            if (array[i] == array[j]) {
-                counter++;
-                if (max < counter)
-                max  = counter;
-                item = array[i];
-            }
-        }
-
-        counter = 0;
+    if (!elems[val]) {
+      elems[val] = 1;
+    } else {
+      elems[val]++;
     }
 
-    const alert = document.getElementsByClassName('alert');
+    for (let j = i; j < array.length; j++) {
+      if (array[i] == array[j]) {
+        counter++;
+        if (max < counter) max = counter;
+        item = array[i];
+      }
+    }
 
-    alert.classList.remove('d-none');
-    alert.classList.add('d-flex');
+    counter = 0;
+  }
 
-    alert.querySelector('span:first-child').innerText = item;
-    alert.querySelector('span:last-child').innerText = max;
+  const alert = document.querySelector(".alert"); /* errore: il selettore corretto è query selector e manca il punto prima della classe */
 
-    console.log('${item} trovato ${max} volte');
+  alert.classList.remove("d-none");
+  alert.classList.add("d-flex");
+
+  alert.querySelector("span:first-child").textContent = item;
+  alert.querySelector("span:last-child").textContent = max;
+
+  console.log(`${item} trovato ${max} volte`); /* errore: c'erano gli apici normali e non i backtick */
 });
